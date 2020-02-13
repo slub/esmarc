1ffdcbf5eaeb45a609464fd00bd0c243868a2393
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-02-13 11:31:37 +0100

    hotfix for mapping bug found due to link checker

--- esmarc/swb_fix.py
+++ esmarc/swb_fix.py
@@ -12,6 +12,26 @@ isil2sameAs = {
 }
 
 
+map_entities = {
+    "p": "persons",  # Personen, individualisiert
+    "n": "persons",  # Personen, namen, nicht individualisiert
+    "s": "topics",  # Schlagwörter/Berufe
+    "b": "organizations",  # Organisationen
+    "g": "geo",  # Geographika
+    "u": "works",  # Werktiteldaten
+    "f": "events"
+}
+
+map_types = {
+    "p": "Person",  # Personen, individualisiert
+    "n": "Person",  # Personen, namen, nicht individualisiert
+    "s": "Thing",  # Schlagwörter/Berufe
+    "b": "Organization",  # Organisationen
+    "g": "Place",  # Geographika
+    "u": "CreativeWork",  # Werktiteldaten
+    "f": "Event"
+}
+
 marc2relation = {
     "VD-16 Mitverf": "contributor",
     "v:Mitverf": "contributor",
