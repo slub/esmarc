7b2e35dd7fa566c555f3f5c38c7be49ee126f25a
bhering <bernhard.hering@slub-dresden.de>
2022-05-13 09:03:41 +0200

    refactoring of lookup tables

--- esmarc/swb_fix.py
+++ esmarc/swb_fix.py
@@ -1,14 +1,44 @@
-isil2sameAs = {
-    "(DE-627)": "http://swb.bsz-bw.de/DB=2.1/PPNSET?PPN=",
-    "DE-627": "http://swb.bsz-bw.de/DB=2.1/PPNSET?PPN=",
-    # "DE-576":"http://swb.bsz-bw.de/DB=2.1/PPNSET?PPN=",
-    # "(DE-576)":"http://swb.bsz-bw.de/DB=2.1/PPNSET?PPN=",
-    "DE-588": "https://d-nb.info/gnd/",
-    "(DE-588)": "https://d-nb.info/gnd/",
-    "DE-601": "http://gso.gbv.de/PPN?PPN=",
-    "(DE-601)": "http://gso.gbv.de/PPN?PPN=",
-    "DE-15": "Univeristätsbibliothek Leipzig",
-    "DE-14": "Sächsische Landesbibliothek – Staats- und Universitätsbibliothek Dresden",
+
+lookup_sameAs = {
+    "(DE-588)":{
+        "@id": "https://d-nb.info/",
+        "publisher": {"preferredName": "Deutsche Nationalbibliothek",
+                      "@id": "https://ld.zdb-services.de/resource/organisations/DE-101",
+                      "abbr": "DNB"}
+        },
+    "(DE-627)": {
+        "@id": "https://opac.k10plus.de/DB=2.299/PPNSET?PPN=",
+        "publisher": {"preferredName": "K10Plus",
+                      "@id": "https://data.slub-dresden.de/organizations/103302212",
+                      "abbr": "KXP"}
+        },
+    "(DE-576)": {
+        "@id": "https://swb.bsz-bw.de/DB=2.1/PPNSET?PPN=",
+        "publisher": {
+            "@id": "https://ld.zdb-services.de/resource/organisations/DE-576",
+            "preferredName": "Bibliotheksservice-Zentrum Baden-Württemberg (BSZ)",
+            "abbr": "BSZ"
+        }
+    },
+    "(DE-600)": {
+        "@id": "https://ld.zdb-services.de/resource/",
+        "publisher": {
+            "@id": "https://ld.zdb-services.de/resource/organisations/DE-600",
+            "preferredName": "Zeitschriftendatenbank (ZDB)",
+            "abbr": "ZDB"
+        }
+    },
+    "(DE-601)": {
+        "@id": "http://gso.gbv.de/PPN?PPN="
+            },
+    "(DE-633)": {
+        "@id": "https://opac.rism.info/id/rismid/",
+        "publisher": {
+            "@id": "https://ld.zdb-services.de/resource/organisations/DE-633",
+            "preferredName": "Répertoire International des Sources Musicales",
+            "abbr": "RISM"
+        }
+    }
 }
 
 
