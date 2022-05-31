66d1b5714e1060cd49f0e7097cca3acacec753df
boerni667 <bernhard.hering@slub-dresden.de>
2022-05-31 12:38:44 +0200

    rewrote some stuff due to changes for elasticsearch 8

--- setup.py
+++ setup.py
@@ -38,9 +38,10 @@ setup(name='esmarc',
       },
       install_requires=[
           'argparse>=1.4.0',
-          'elasticsearch>=5.0.0',
+          'elasticsearch>=8.0.0',
           'rdflib>=4.2.2',
-          'dateparser'
+          'dateparser',
+          'urllib"
       ],
       python_requires=">=3.5.*",
       entry_points={
