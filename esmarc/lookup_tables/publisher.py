66dafe0f212db126f9f80c7d5f68a8c9a0d89ca6
boerni667 <bernhard.hering@slub-dresden.de>
2022-12-08 17:12:32 +0100

    DMG-1343: added lookup_table

---
+++ esmarc/lookup_tables/publisher.py
@@ -0,0 +1,5 @@
+places = {"dbp": "Distribution place",
+          "mfp": "Manufacture place",
+          "prp": "Production place",
+          "pup": "Publication place",
+          "uvp": "University place"}
